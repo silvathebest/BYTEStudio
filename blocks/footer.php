b3129d0d2da644ecb91ae694c68525e64eb6344b
silvathebest <silverchat228@gmail.com>
2020-03-12 22:08:43 +0400

    base

---
+++ blocks/footer.php
@@ -0,0 +1,8 @@
+<footer>
+        <div class="social">
+            <span>BYTE Studio</span>
+            <a href=""><img src="img/gmail.png" alt="gmail"></a>
+            <a href="https://vk.com/byte_studio"><img src="img/vk.png" alt="vk"></a>
+        </div>
+    </footer>
+</body>
---
+++ blocks/header.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="header__logo"><span>BYTE Studio</span></div>
+    <nav>
+        <div class="topnav" id="myTopnav">
+            <a href="#">ГЛАВНЯ</a>
+            <a href="#">О НАС</a>
+            <a href="#">ПОРТФОЛИО</a>
+            <a href="#">КОНТАКТЫ</a>
+            <a href="#" id="menu" class="icon">&#9776;</a>
+        </div>
+    </nav>
+</header>
