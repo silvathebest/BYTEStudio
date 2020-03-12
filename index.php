0800a548c73fc7f217a19415f5593129d0f0dcc3
silvathebest <silverchat228@gmail.com>
2020-03-12 21:24:16 +0400

    start

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Anton|Exo+2:400,700,800,900&display=swap&subset=cyrillic,cyrillic-ext"
+          rel="stylesheet">
+    <title>BYTEStudio</title>
+
+</head>
+<body>
+<header>
+    <div class="header__logo"><span>BYTE Studio</span></div>
+    <nav>
+        <div class="topnav" id="myTopnav">
+            <a href="#">ГЛАВНЯ</a>
+            <a href="#">О НАС</a>
+            <a href="#">ПОРТФОЛИО</a>
+            <a href="#">КОНТАКТЫ</a>
+            <a href="#" id="menu" class="icon">&#9776;</a>
+        </div>
+    </nav>
+</header>
+<div class="welcome">
+    <div class="left_welcome">
+        <h2>BYTE Studio</h2>
+        <b>Разработка сайтов <br>
+            Любой сложности</b>
+        <p>Создание сайтов для любого вида бизнеса <br>
+            Разработка программного обеспечения <br>
+            Полная адаптивность под все устройства <br>
+            Настройка и сопровождение сайта <br>
+        </p>
+        <button>Связаться с нами</button>
+    </div>
+    <div class="right_welcome">
+    </div>
+</div>
+<footer>
+    <div class="social">
+        <span>BYTE Studio</span>
+        <a href=""><img src="img/gmail.png" alt="gmail"></a>
+        <a href="https://vk.com/byte_studio"><img src="img/vk.png" alt="vk"></a>
+    </div>
+</footer>
+<script src="js/script.js"></script>
+</body>
+</html>
