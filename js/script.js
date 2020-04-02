48bca9771c4300493adf52e99a862ce0daa2ce8d
silvathebest <silverchat228@gmail.com>
2020-04-03 01:00:23 +0400

    Заготовка под мобильное меню, заготовка прелоадера

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
-$('.calc__site__item').on('click',function(){
+window.onload = function () {
+    let preloader = document.getElementById('preloader');
+    preloader.style.display = 'none';
+};
+
+
+$('.calc__site__item').on('click', function () {
     $('.calc__site__item').children().removeClass('check');
     $('.calc__site__item').removeClass('calc__site__item__active');
-    if(!$(this).children().hasClass('check')){
+    if (!$(this).children().hasClass('check')) {
         $(this).children().addClass('check');
         $(this).addClass('calc__site__item__active');
-    }else{
+    } else {
         $(this).removeClass('calc__site__item__active');
         $(this).children().removeClass('check');
     }
 });
 
-$('#calc_next_view_2').on('click',function(){
-    if($('.calc__site__item').children().hasClass('check')){
+$('#calc_next_view_2').on('click', function () {
+    if ($('.calc__site__item').children().hasClass('check')) {
         $('.calc_view_1').addClass('hidden');
         $('.calc_view_2').removeClass('hidden');
         $('#selected__site').text();
-    }else{
-        func_show_tooltip(obj,true);
+    } else {
+        func_show_tooltip(obj, true);
     }
 });
 
@@ -24,49 +30,50 @@ $('#calc_next_view_2').on('click',function(){
 let timer;
 
 $(".calc__site__item").on({
-    mouseenter: function(e){
+    mouseenter: function (e) {
         let obj = $(this);
         clearTimeout(timer);
-        timer = setTimeout(function(){
-            func_show_tooltip(obj,true,e);
+        timer = setTimeout(function () {
+            func_show_tooltip(obj, true, e);
         }, 1000);
     },
-    mouseleave: function(){
+    mouseleave: function () {
         clearTimeout(timer);
-        func_show_tooltip(null,false);
+        func_show_tooltip(null, false);
     }
 });
 
-$(window).scroll(function(e){
+$(window).scroll(function (e) {
     let scrollTop = $(window).scrollTop();
-    if(scrollTop > 400){
+    if (scrollTop > 400) {
         $('nav').fadeIn();
-    }else{
+    } else {
         $('nav').fadeOut();
     }
 });
+
 //Функция показа тултипа
-function func_show_tooltip(obj,bool,){
-   let tooltip = $('.tooltip');
-   
-   var width = tooltip.outerWidth(true);
-   if(bool){
-    let html = $('h5:contains('+obj[0].innerText+')').next('ul').clone();
-    tooltip.css('top',(obj[0].offsetTop + 80) + 'px');
-    tooltip.css('left',(obj[0].offsetLeft + width /2) + 'px');
-    var all = $(window).width();
-    var left = obj[0].offsetLeft;
-    var offset = all - (left + width);
-    if(offset < 30){
-        tooltip.css('top',(obj[0].offsetTop + 80) + 'px');
-        tooltip.css('left',(obj[0].offsetLeft - width ) + 'px');
+function func_show_tooltip(obj, bool,) {
+    let tooltip = $('.tooltip');
+
+    var width = tooltip.outerWidth(true);
+    if (bool) {
+        let html = $('h5:contains(' + obj[0].innerText + ')').next('ul').clone();
+        tooltip.css('top', (obj[0].offsetTop + 80) + 'px');
+        tooltip.css('left', (obj[0].offsetLeft + width / 2) + 'px');
+        var all = $(window).width();
+        var left = obj[0].offsetLeft;
+        var offset = all - (left + width);
+        if (offset < 30) {
+            tooltip.css('top', (obj[0].offsetTop + 80) + 'px');
+            tooltip.css('left', (obj[0].offsetLeft - width) + 'px');
+        }
+        if (html) {
+            tooltip.html(html);
+            tooltip.fadeIn();
+        }
+    } else {
+        tooltip.fadeOut();
     }
-    if(html){
-        tooltip.html(html);
-        tooltip.fadeIn();
-    }
-   }else{
-    tooltip.fadeOut();
-   }
 }
 
